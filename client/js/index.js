db0ca408e5f634057127b45e23ec7938d0a643f6
Your Namecarlo <carlo.jesena@gmail.com>
2016-12-16 13:09:48 -0800

    added reducers and store, no errors in terminal

--- client/js/index.js
+++ client/js/index.js
@@ -1,10 +1,16 @@
 import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
+// imports for store
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
 // import {connect, Provider} from 'react-redux';
 import CheeseList from '../components/cheese-list';
+import * as reducers from '../reducers/cheese';
 
 console.log(`Client running in ${process.env.NODE_ENV} mode`);
+
+export default createStore(reducers.cheeseReducer, applyMiddleware(thunk));
 
 
 document.addEventListener('DOMContentLoaded', () => {
--- client/reducers/cheese.js
+++ client/reducers/cheese.js
@@ -8,6 +8,19 @@ const intital = {
 
 export const cheeseReducer = function(state = intital, action) {
 
-  if (action)
+  if (action.type === FETCH_CHEESES_REQUEST) {
+    return {...state,
+      loading:true}
+  }
+  else if (action.type === FETCH_CHEESES_SUCCESS) {
+    return {...state,
+      cheeses:action.cheeses,
+      loading: false,
+      error:null}
+  }
+  else if (action.type === FETCH_CHEESES_ERROR) {
+    return {...state,
+      loading: false,
+      error: action.error}
   }
 }
